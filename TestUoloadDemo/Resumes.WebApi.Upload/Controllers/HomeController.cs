595f6cd1169e84b893915f7e0949938e27cb7881
CKottapalli <chandra.kottapalli@gmail.com>
2025-02-03 14:28:02 -0500

    Updated the security features

--- TestUoloadDemo/Resumes.WebApi.Upload/Controllers/HomeController.cs
+++ TestUoloadDemo/Resumes.WebApi.Upload/Controllers/HomeController.cs
@@ -8,26 +8,26 @@ namespace Resumes.WebApi.Upload.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
 
     }
 }
--- TestUoloadDemo/Resumes.WebApi.Upload/Controllers/ResumeController.cs
+++ TestUoloadDemo/Resumes.WebApi.Upload/Controllers/ResumeController.cs
@@ -4,12 +4,40 @@ namespace Resumes.WebApi.Upload.Controllers
 {
     public class ResumeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        [HttpGet]
+        //[HttpGet]
+        //public IActionResult ResumeUpload()
+        //{
+        //    string BasePath = @"C:\Chandra\Resumes";
+
+        //    if (!Directory.Exists(BasePath))
+        //    {
+        //        Directory.CreateDirectory(BasePath);
+        //    }
+
+        //    string relativePath = @"Resumes\" + DateTime.Now.ToString("yyyMMdd");
+
+        //    string filesToUpload = Path.Combine(BasePath, relativePath);
+
+        //    if (!Directory.Exists(filesToUpload))
+        //    {
+        //        Directory.CreateDirectory(filesToUpload);
+        //    }
+
+        //    FileInfo[] files = new DirectoryInfo(filesToUpload).GetFiles();
+
+        //    if (files != null)
+        //        Console.WriteLine($"Total Files: {files.Length}");
+
+        //    return View();
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult ResumeUpload()
         {
             string BasePath = @"C:\Chandra\Resumes";
