9d501a6238bbb7482512199a861b8153dfccc3d4
CKottapalli <chandra.kottapalli@gmail.com>
2025-02-03 14:03:22 -0500

    Initial check in

---
+++ TestUoloadDemo/ResumeFileUpload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string BasePath = @"C:\Chandra\Resumes";
+
+if(!Directory.Exists(BasePath))
+{
+    Directory.CreateDirectory(BasePath);
+}
+
+string relativePath = @"Resumes\" + DateTime.Now.ToString("yyyMMdd");
+
+string filesToUpload = Path.Combine(BasePath, relativePath);
+
+if (!Directory.Exists(filesToUpload))
+{
+    Directory.CreateDirectory(filesToUpload);
+}
+
+FileInfo[] files =  new DirectoryInfo(filesToUpload).GetFiles();
+
+if (files != null)
+    Console.WriteLine($"Total Files: {files.Length}");
